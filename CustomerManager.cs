b82112c9cee32fa7715ab9a3ce587b71ed2401ab
sensoyyasin <hallowiertndio@gmail.com>
2021-02-01 22:08:24 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        Customer NewCustomer = new Customer();
+        public void AddCustomer(List<Customer> customers)
+        {
+            Console.WriteLine("Please enter your Id ");
+            NewCustomer.Id = Console.ReadLine();
+
+            Console.WriteLine("Please enter your name ");
+            NewCustomer.Name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your surname ");
+            NewCustomer.Surname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age ");
+            NewCustomer.Age = Console.ReadLine();
+            customers.Add(NewCustomer);
+
+        }
+
+        public void ListCustomer(List<Customer> customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customers are listed ");
+                Console.WriteLine("Customers Id " + customer.Id);
+                Console.WriteLine("Customers Name " + customer.Name);
+                Console.WriteLine("Customers Surname " + customer.Surname);
+                Console.WriteLine("Customers Age " + customer.Age);
+                Console.WriteLine("*********");
+            }
+        }
+
+        public void RemoveCustomer(Customer customer)
+        {
+            Console.WriteLine("Customers removed " + customer.Id + " / " + customer.Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to banking");
+            Console.WriteLine("**");
+            Console.WriteLine("**");
+            Console.WriteLine("**");
+            Console.WriteLine("**");
+
+            Customer customer1 = new Customer();
+            customer1.Id = "Apexs";
+            customer1.Name = "Yasin";
+            customer1.Surname = "Sensoy";
+            customer1.Age = "19";
+
+            Customer customer2 = new Customer();
+            customer2.Id = "xax";
+            customer2.Name = "yay";
+            customer2.Surname =  "zaz";
+            customer2.Age = "91";
+
+
+
+            List<Customer> customers = new List<Customer>();
+            customers.Add(customer1);
+            customers.Add(customer2);
+
+            Console.WriteLine("customers.Count");
+            CustomerManager customerManager = new CustomerManager();
+            Console.WriteLine(customers.Count);
+
+            foreach(var customer in customers)
+            {
+                Console.WriteLine(customer.Id + " * " + customer.Name + " * " + customer.Surname + " * " + customer.Age);
+            }
+
+            customerManager.AddCustomer(customers);
+            customerManager.ListCustomer(customers);
+            Console.WriteLine(customers.Count);
+            foreach(var customer in customers)
+            {
+                Console.WriteLine(customer.Id + " * " + customer.Name + " * " + customer.Surname + " * " + customer.Age);
+
+            }
+        }
+    }
+}
